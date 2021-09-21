06a171775cadb9ded83ed730d5d5fc812d4d992c
OliverLSanz <oliverlsanz@gmail.com>
2021-09-21 12:25:59 +0200

    printing hello world

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Printing;
 
 namespace Playroom_Kiosk
 {
@@ -20,6 +21,35 @@ namespace Playroom_Kiosk
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>  
+        /// This method creates a dynamic FlowDocument. You can add anything to this  
+        /// FlowDocument that you would like to send to the printer  
+        /// </summary>  
+        /// <returns></returns>  
+        private FlowDocument CreateFlowDocument()
+        {
+            // Create a FlowDocument  
+            FlowDocument doc = new FlowDocument();
+            // Create a Section  
+            Section sec = new Section();
+            // Create first Paragraph  
+            Paragraph p1 = new Paragraph();
+            // Create and add a new Bold, Italic and Underline  
+            Bold bld = new Bold();
+            bld.Inlines.Add(new Run("Hello World"));
+            Italic italicBld = new Italic();
+            italicBld.Inlines.Add(bld);
+            Underline underlineItalicBld = new Underline();
+            underlineItalicBld.Inlines.Add(italicBld);
+            // Add Bold, Italic, Underline to Paragraph  
+            p1.Inlines.Add(underlineItalicBld);
+            // Add Paragraph to Section  
+            sec.Blocks.Add(p1);
+            // Add Section to FlowDocument  
+            doc.Blocks.Add(sec);
+            return doc;
+        }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,11 +57,15 @@ namespace Playroom_Kiosk
 
         private void ButtonAddName_Click(object sender, RoutedEventArgs e)
         {
-            if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text))
-            {
-                lstNames.Items.Add(txtName.Text);
-                txtName.Clear();
-            }
+            // Create a PrintDialog  
+            PrintDialog printDlg = new PrintDialog();
+            // Create a FlowDocument dynamically.  
+            FlowDocument doc = CreateFlowDocument();
+            doc.Name = "FlowDoc";
+            // Create IDocumentPaginatorSource from FlowDocument  
+            IDocumentPaginatorSource idpSource = doc;
+            // Call PrintDocument method to send document to printer  
+            printDlg.PrintDocument(idpSource.DocumentPaginator, "Hello WPF Printing.");
         }
     }
 }
